c2204498ab2bed95a0cd02f075cbfbceb33cc310
robertozdoba <rozzy1993@gmail.com>
2020-03-16 14:30:42 -0700

    added justin username or statement

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,7 +15,7 @@ exports.login = (req, res, next) => {
 
 exports.loginAttempt = (req, res, next) => {
     let {username, password} = req.body;
-    if((username === myUsername || robUsername === justinUsername) && password === myPassword) {
+    if((username === robUsername || username === justinUsername) && password === myPassword) {
         res.redirect(301, '/artists');
     } else {
         res.redirect(301, '/login');
