2b034aea13c13f58784a72e11df52b51c2338082
robertozdoba <rozzy1993@gmail.com>
2020-03-29 16:21:57 -0700

    added to more tests and modified loginTest

--- tests/searchArtistTest.js
+++ tests/searchArtistTest.js
@@ -1,7 +1,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
-describe('loginTest', function() {
+describe('searchArtistTest', function() {
   this.timeout(30000)
   let driver
   let vars
@@ -12,15 +12,12 @@ describe('loginTest', function() {
   afterEach(async function() {
     await driver.quit();
   })
-  it('loginTest', async function() {
-    await driver.get("https://cloudfinalproject.herokuapp.com/")
+  it('searchArtistTest', async function() {
+    await driver.get("https://cloudfinalproject.herokuapp.com/artists")
     await driver.manage().window().setRect(1280, 687)
-    await driver.findElement(By.id("username-input")).click()
-    await driver.findElement(By.id("username-input")).sendKeys("A01047477")
-    await driver.findElement(By.id("password-input")).click()
-    await driver.findElement(By.id("password-input")).sendKeys("password")
-    await driver.findElement(By.id("login-button")).click()
     await driver.findElement(By.id("search-input")).click()
+    await driver.findElement(By.id("search-input")).sendKeys("Lego")
     await driver.findElement(By.id("search-button")).click()
+    await driver.findElement(By.css(".artist-description")).click()
   })
 })
