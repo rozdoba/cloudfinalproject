f49e72dc2256ebe82f0d33421ca5fba7c17aff34
robertozdoba <rozzy1993@gmail.com>
2020-03-29 22:42:20 -0700

    combined all tests into one webTests.js

--- selenium-tests.sh
+++ selenium-tests.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-node ./tests/loginTest >> selenium-test-results.txt
-node ./tests/addArtistTest >> selenium-test-results.txt
-node ./tests/searchArtistTest >> selenium-test-results.txt
-node ./tests/deleteArtistTest >> selenium-test-results.txt
-node ./tests/logoutTest >> selenium-test-results.txt
-
-cat selenium-test-results.txt
+node ./tests/webTests > selenium-test-results.log
---
+++ tests/webTests.js
@@ -0,0 +1,102 @@
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+const chrome = require('selenium-webdriver/chrome')
+
+const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().addArguments('--headless'))
+    .build();
+
+const loginTest = async () => {
+  await driver.get("https://cloudfinalproject.herokuapp.com/")
+  await driver.manage().window().setRect(1280, 687)
+  await driver.findElement(By.id("username-input")).click()
+  await driver.findElement(By.id("username-input")).sendKeys("A01047477")
+  await driver.findElement(By.id("password-input")).click()
+  await driver.findElement(By.id("password-input")).sendKeys("password")
+  await driver.findElement(By.id("login-button")).click()
+  await driver.findElement(By.id("search-input")).click()
+  await driver.findElement(By.id("search-button")).click()
+  return true;
+}
+
+const addArtistTest = async () => {
+    await driver.get("https://cloudfinalproject.herokuapp.com/artists")
+    await driver.manage().window().setRect(1280, 687)
+    await driver.findElement(By.id("add-button")).click()
+    await driver.findElement(By.name("name")).click()
+    await driver.findElement(By.name("name")).sendKeys("Lego Man")
+    await driver.findElement(By.name("description")).click()
+    await driver.findElement(By.name("description")).sendKeys("I am Lego Man")
+    await driver.findElement(By.name("url")).click()
+    await driver.findElement(By.name("url")).sendKeys("https://randomuser.me/api/portraits/lego/6.jpg")
+    await driver.findElement(By.id("submit-button")).click()
+    await driver.findElement(By.id("add-button")).click()
+    await driver.findElement(By.name("name")).click()
+    await driver.findElement(By.name("name")).sendKeys("Human")
+    await driver.findElement(By.name("description")).click()
+    await driver.findElement(By.name("description")).sendKeys("I am Human")
+    await driver.findElement(By.name("url")).click()
+    await driver.findElement(By.name("url")).sendKeys("https://randomuser.me/api/portraits/women/24.jpg")
+    await driver.findElement(By.id("submit-button")).click()
+    return true;
+}
+
+const searchArtistTest = async () => {
+    await driver.get("https://cloudfinalproject.herokuapp.com/artists")
+    await driver.manage().window().setRect(1280, 687)
+    await driver.findElement(By.id("search-input")).click()
+    await driver.findElement(By.id("search-input")).sendKeys("Lego")
+    await driver.findElement(By.id("search-button")).click()
+    await driver.findElement(By.css(".artist-description")).click()
+    return true;
+}
+
+const deleteArtistTest = async () => {
+    await driver.get("https://cloudfinalproject.herokuapp.com/artists")
+    await driver.manage().window().setRect(1280, 687)
+    await driver.findElement(By.css("#delete-artist-form > button")).click()
+    return true;
+}
+
+const logoutTest = async () => {
+    await driver.get("https://cloudfinalproject.herokuapp.com/artists")
+    await driver.manage().window().setRect(1280, 692)
+    await driver.findElement(By.id("logout-button")).click()
+    await driver.findElement(By.id("username-input")).click()
+    await driver.findElement(By.id("password-input")).click()
+    await driver.findElement(By.id("login-button")).click()
+    return true;
+}
+
+(async () => {
+  try {
+        if(await loginTest()) {
+            console.log("LoginTest SUCCESS");
+        }
+
+        if(await addArtistTest()) {
+            console.log("addArtistTest SUCCESS");
+        }
+
+        if(await searchArtistTest()) {
+            console.log("searchArtistTest SUCCESS");
+        }
+
+        if(await deleteArtistTest()) {
+            console.log("deleteArtistTest SUCCESS");
+        }
+
+        if(await logoutTest()) {
+            console.log("logoutTest SUCCESS");
+        }
+        
+        await driver.quit()
+
+  } catch (error) {
+    await driver.quit();
+    console.log("Selenium Workflow FAILED")
+  }
+
+})()
